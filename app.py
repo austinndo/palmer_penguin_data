5d15948cd068a182857b00d445acd5c4f8a4c1b9
Austin Do <austinndo@gmail.com>
2023-05-07 18:52:45 -0700

    Data cleaned a bit, setting up dash app now. Correctly rendering title of app

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import pandas as pd
 import plotly.express as px
+from dash import Dash, html, dcc, callback, Output, Input
 
 # Read the penguin size data
 df = pd.read_csv('./data/penguins_size.csv')
 # Remove rows with empty cells 'NaN'
 df = df.dropna()
 
-# View
-print(df)
+# Initialize dash app
+app = Dash(__name__)
+
+# Set layout
+app.layout = html.Div([
+    html.Div(children='Palmer Penguin Data')
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+# # View
+# print(df)
