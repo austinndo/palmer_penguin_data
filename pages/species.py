131f2ba234274f0946e6c3d7e16d03d855ea23af
Austin Do <austinndo@gmail.com>
2023-05-08 17:38:01 -0700

    Resolving issue with callback, Output, Input

--- pages/species.py
+++ pages/species.py
@@ -1,7 +1,7 @@
 import dash
 import dash_bootstrap_components as dbc
 import plotly_express as px
-from dash import html, dcc
+from dash import html, dcc, callback, Output, Input
 from .home import df
 
 dash.register_page(__name__)
@@ -9,4 +9,16 @@ dash.register_page(__name__)
 
 layout = html.Div([
     html.H1("Species graph"),
-    dcc.Graph(figure=px.scatter(df, x='species', y='culmen_length_mm'))])
+    dcc.RadioItems(options=['Adelie', 'Chinstrap',
+                   'Gentoo'], value='Adelie', id='controls-and-radio-item'),
+    # dcc.Graph(figure=px.scatter(df, x='species', y='culmen_length_mm'))
+    dcc.Graph(figure={}, id='controls-and-graph')
+])
+
+
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value'))
+def update_graph(species):
+    fig = px.scatter(df, x='culmen_length_mm', y=species)
+    return fig
