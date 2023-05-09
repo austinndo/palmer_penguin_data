813915d16cfa3ee4c2033efab5850d0811b0ead8
Austin Do <austinndo@gmail.com>
2023-05-08 17:39:12 -0700

    radio items are available, went back to old scatterplot for now

--- pages/species.py
+++ pages/species.py
@@ -11,14 +11,14 @@ layout = html.Div([
     html.H1("Species graph"),
     dcc.RadioItems(options=['Adelie', 'Chinstrap',
                    'Gentoo'], value='Adelie', id='controls-and-radio-item'),
-    # dcc.Graph(figure=px.scatter(df, x='species', y='culmen_length_mm'))
-    dcc.Graph(figure={}, id='controls-and-graph')
+    dcc.Graph(figure=px.scatter(df, x='species', y='culmen_length_mm'))
+    # dcc.Graph(figure={}, id='controls-and-graph')
 ])
 
 
-@callback(
-    Output(component_id='controls-and-graph', component_property='figure'),
-    Input(component_id='controls-and-radio-item', component_property='value'))
-def update_graph(species):
-    fig = px.scatter(df, x='culmen_length_mm', y=species)
-    return fig
+# @callback(
+#     Output(component_id='controls-and-graph', component_property='figure'),
+#     Input(component_id='controls-and-radio-item', component_property='value'))
+# def update_graph(species):
+#     fig = px.scatter(df, x='culmen_length_mm', y=species)
+#     return fig
