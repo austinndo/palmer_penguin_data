38769fd7f6d43fbf2a41ae61652ca435fa5d8980
Austin Do <austinndo@gmail.com>
2023-05-11 00:28:02 -0700

    Fixed issue. Correctly rendering graphs with tabs! Now need to create dataframe for species, island, sex

--- pages/body_mass.py
+++ pages/body_mass.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import dash
 import plotly_express as px
-from dash import callback, dcc, html, Input, Output
+from dash import callback, dcc, html, Input, Output, dash_table
 
 
 df = pd.read_csv('csv_files/penguins_lter.csv')
@@ -21,24 +21,34 @@ def description():
 dash.register_page(__name__, path='/body_mass',
                    title=title, description=description)
 
+# species_fig = px.scatter(
+#     data_frame=df, x=[0, 1, 2, 3, 4, 5], y=[5, 12, 14, 6, 7, 8])
+
+species_fig = px.scatter(
+    data_frame=df, x=df['Sample Number'], y=df['Body Mass (g)'])
+
+# Trying to return a figure in the children of html.Div([ ]) seems to cause errors. "An object was provided as `children` instead of a component, string, or number... ""
 layout = html.Div([
     dcc.Tabs(id='body_mass_tabs', value='species', children=[
         dcc.Tab(label='Species', value='species'),
         dcc.Tab(label='Island', value='island'),
         dcc.Tab(label='Sex', value='sex'),
     ]),
-    html.Div(id='body_mass_content')
+    html.Div(id='body_mass_graphs'),
 ])
 
 
 @callback(
-    Output('body_mass_content', 'children'),
+    Output('body_mass_graphs', 'children'),
     Input('body_mass_tabs', 'value')
 )
 def render(tab):
     if tab == 'species':
         return html.Div([
             html.H3('Body Mass by Species'),
+            dcc.Graph(
+                figure=species_fig
+            )
         ])
     elif tab == 'island':
         return html.Div([
